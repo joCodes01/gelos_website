0abb40df034b617970a166fc18fd215adce8d76c
JoCodes01 <76087277+joCodes01@users.noreply.github.com>
2026-04-16 15:02:28 +1000

    Improve styles. Add animated banner

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,13 @@
 new Glide(".glide").mount();
 
 new Accordion(".accordion-container");
+
+document.getElementById("current-year").innerText = new Date().getFullYear();
+
+//banner
+const heroScroll = document.querySelector(".hero__scroll");
+const introduction = document.getElementById("introduction");
+
+heroScroll.addEventListener("click", () =>
+  introduction.scrollIntoView({ behavior: "smooth", block: "start" }),
+);
